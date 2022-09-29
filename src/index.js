c179419fe5a615778be5cad97c85c8cc0cb45ce0
FDE8 <dflorent.80@gmail.com>
2022-09-29 10:59:38 +0200

    six commit

--- src/index.js
+++ src/index.js
@@ -56,3 +56,59 @@ ReactDOM.render(
   element4,
   document.getElementById('root4')
 );
+
+function App() {
+  return (
+    <div>
+      <Welcome name="Nathalie" />
+      <Welcome name="Cahal" />
+      <Welcome name="Edite" />
+    </div>
+  );
+}
+
+ReactDOM.render(
+  <App />,
+  document.getElementById('root5')
+);
+function formatDate(date) {
+  return date.toLocaleDateString();
+}
+
+function Comment(props) {
+  return (
+    <div className="Comment">
+      <div className="UserInfo">
+        <img
+          className="Avatar"
+          src={props.author.avatarUrl}
+          alt={props.author.name}
+        />
+        <div className="UserInfo-name">
+          {props.author.name}
+        </div>
+      </div>
+      <div className="Comment-text">{props.text}</div>
+      <div className="Comment-date">
+        {formatDate(props.date)}
+      </div>
+    </div>
+  );
+}
+
+const comment = {
+  date: new Date(),
+  text: 'test numéro 6 :)!',
+  author: {
+    name: 'florent',
+    avatarUrl: 'https://placekitten.com/g/64/64',
+  },
+};
+ReactDOM.render(
+  <Comment
+    date={comment.date}
+    text={comment.text}
+    author={comment.author}
+  />,
+  document.getElementById('root6')
+);
