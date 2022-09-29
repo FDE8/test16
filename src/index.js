73b7069235421aeed843d63f1cbe2e2d64942f2e
FDE8 <dflorent.80@gmail.com>
2022-09-29 10:33:07 +0200

    trois commit

--- src/index.js
+++ src/index.js
@@ -2,13 +2,47 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 
-
-
-//const root = ReactDOM.createRoot(document.getElementById('root'));
+const name = 'Clarisse Agbegnenou';
+const element = <h1>Bonjour, {name}</h1>;
 
 ReactDOM.render(
-  <h1>Bonjour, monde !</h1>,
+  element,
   document.getElementById('root')
 );
 
+function formatName(user) {
+  return user.firstName + ' ' + user.lastName;
+}
+
+const user = {
+  firstName: 'Kylian',
+  lastName: 'Mbappé'
+};
+
+const element2 = (
+  <div>
+    <h1>Bonjour, {formatName(user)} !  </h1>
+    <h1>Bonjour, {name}</h1>
+  </div>
+);
+
+ReactDOM.render(
+ 
+  element2,
+  document.getElementById('root')
+);
+
+
+
+
+function tick() {
+  const element3 = (
+    <div>
+      <h1>Bonjour, monde !</h1>
+      <h2>Il est {new Date().toLocaleTimeString()}.</h2>
+    </div>
+  );
+  ReactDOM.render(element3, document.getElementById('root3'));}
+
+setInterval(tick, 1000);
 
