96ec7c16b47613eedaa5d10f35a08beea17f4767
FDE8 <dflorent.80@gmail.com>
2022-09-29 15:09:30 +0200

    liste commit

--- src/index.js
+++ src/index.js
@@ -327,3 +327,83 @@ ReactDOM.render(
   <Page />,
   document.getElementById('root12')
 );
+
+const numbers1 = [1, 2, 3, 4, 5];
+const listItems = numbers1.map((number) =>
+  <li key={number.toString()} >
+    {number}
+  </li>
+  );
+
+ReactDOM.render(
+  <ul>{listItems}</ul>,
+  document.getElementById('root14')
+);
+
+function NumberList(props) {
+  const numbers = props.numbers;
+  const listItems = numbers.map((number) => <li>{number}</li>  );
+  return (
+    <ul>{listItems}</ul>  );
+}
+
+const numbers = [0,1, 2, 3, 4, 5, 8];
+
+ReactDOM.render(
+  <NumberList numbers={numbers} />,
+  document.getElementById('root15')
+);
+
+function ListItem(props) {
+  // Correct ! Pas la peine de spécifier la clé ici :
+  return <li>{props.value}</li>;
+}
+
+function NumberList3(props) {
+  const numbers = props.numbers;
+  const listItems = numbers.map((number) =>
+    // Correct ! La clé doit être spécifiée dans le tableau.
+    <ListItem key={number.toString()} value={number} />  );
+  return (
+    <ul>
+      {listItems}
+    </ul>
+  );
+}
+
+const numbers3 = [10, 20, 30, 40, 50];
+ReactDOM.render(
+  <NumberList3 numbers={numbers3} />,
+  document.getElementById('root16')
+);
+
+function Blog(props) {
+  const sidebar = (<ul>
+      {props.posts.map((post) =>
+        <li key={post.id}>
+          {post.title}
+        </li>
+      )}
+    </ul>
+  );
+  const content = props.posts.map((post) =>    
+    <div key={post.id}>
+      <h3>{post.title}</h3>
+      <p>{post.content}</p>
+    </div>
+  );
+  return (
+    <div>
+      {sidebar}      <hr />
+      {content}    </div>
+  );
+}
+
+const posts = [
+  {id: 1, title: 'Bonjour, monde', content: 'Bienvenue sur la doc de React !'},
+  {id: 2, title: 'Installation', content: 'Vous pouvez installer React depuis npm.'}
+];
+ReactDOM.render(
+  <Blog posts={posts} />,
+  document.getElementById('root17')
+);
