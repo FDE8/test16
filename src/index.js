622f3cfa5609221550d24888b720c4b7f0dc10aa
FDE8 <dflorent.80@gmail.com>
2022-10-02 20:56:51 +0200

    formulaire

--- src/index.js
+++ src/index.js
@@ -371,7 +371,7 @@ function NumberList3(props) {
   );
 }
 
-const numbers3 = [10, 20, 30, 40, 50];
+const numbers3 = [1000, 200, 300, 400, 500];
 ReactDOM.render(
   <NumberList3 numbers={numbers3} />,
   document.getElementById('root16')
@@ -407,3 +407,145 @@ ReactDOM.render(
   <Blog posts={posts} />,
   document.getElementById('root17')
 );
+
+class NameForm extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = { value: '' };
+
+        this.handleChange = this.handleChange.bind(this);
+        this.handleSubmit = this.handleSubmit.bind(this);
+    }
+
+    handleChange(event) {
+        this.setState({ value: event.target.value });
+    }
+
+    handleSubmit(event) {
+        alert('A name was submitted: ' + this.state.value);
+        event.preventDefault();
+    }
+
+    render() {
+        return (
+            <form onSubmit={this.handleSubmit}>
+                <label>
+                    Name:
+                    <input type="text" value={this.state.value} onChange={this.handleChange} />
+                </label>
+                <input type="submit" value="Submit" />
+            </form>
+        );
+    }
+}
+
+const root18 = ReactDOM.createRoot(document.getElementById('root18'));
+root18.render(<NameForm />);
+
+class EssayForm extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = { value: 'Écrivez un essai à propos de votre élément du DOM préféré' };
+        this.handleChange = this.handleChange.bind(this);
+        this.handleSubmit = this.handleSubmit.bind(this);
+    }
+
+    handleChange(event) { this.setState({ value: event.target.value }); }
+    handleSubmit(event) {
+        alert('Un essai a été envoyé : ' + this.state.value);
+        event.preventDefault();
+    }
+
+    render() {
+        return (
+            <form onSubmit={this.handleSubmit}>
+                <label>
+                    Essay:
+                    <textarea value={this.state.value} onChange={this.handleChange} />        </label>
+                <input type="submit" value="Envoyer" />
+            </form>
+        );
+    }
+}
+
+const root19 = ReactDOM.createRoot(document.getElementById('root19'));
+root19.render(<EssayForm />);
+
+class FlavorForm extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = { value: 'coconut' };
+        this.handleChange = this.handleChange.bind(this);
+        this.handleSubmit = this.handleSubmit.bind(this);
+    }
+
+    handleChange(event) { this.setState({ value: event.target.value }); }
+    handleSubmit(event) {
+        alert('Votre parfum favori est : ' + this.state.value);
+        event.preventDefault();
+    }
+
+    render() {
+        return (
+            <form onSubmit={this.handleSubmit}>
+                <label>
+                    Choisissez votre parfum favori :
+                    <select value={this.state.value} onChange={this.handleChange}>            <option value="grapefruit">Pamplemousse</option>
+                        <option value="lime">Citron vert</option>
+                        <option value="coconut">Noix de coco</option>
+                        <option value="mango">Mangue</option>
+                    </select>
+                </label>
+                <input type="submit" value="Envoyer" />
+            </form>
+        );
+    }
+}
+const root20 = ReactDOM.createRoot(document.getElementById('root20'));
+root20.render(<FlavorForm />);
+
+class Reservation extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            isGoing: true,
+            numberOfGuests: 2
+        };
+
+        this.handleInputChange = this.handleInputChange.bind(this);
+    }
+
+    handleInputChange(event) {
+        const target = event.target;
+        const value = target.type === 'checkbox' ? target.checked : target.value;
+        const name = target.name;
+        this.setState({
+            [name]: value
+        });
+    }
+
+    render() {
+        return (
+            <form>
+                <label>
+                    Participe :
+                    <input
+                        name="isGoing" type="checkbox"
+                        checked={this.state.isGoing}
+                        onChange={this.handleInputChange} />
+                </label>
+                <br />
+                <label>
+                    Nombre d'invités :
+                    <input
+                        name="numberOfGuests" type="number"
+                        value={this.state.numberOfGuests}
+                        onChange={this.handleInputChange} />
+                </label>
+            </form>
+        );
+    }
+}
+
+const root21 = ReactDOM.createRoot(document.getElementById('root21'));
+root21.render(<Reservation />);
